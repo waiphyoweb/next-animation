0d3fe6950a5cf476dc0fe2bcad976bbe861a0ee5
WaiPhyoOo <waiphyo24262@gmail.com>
2023-06-08 21:20:55 +0630

    finish nova animation

--- pages/index.js
+++ pages/index.js
@@ -41,15 +41,30 @@ const Animation = () => {
         >
           <Image src='./images/bottom right small meterior.svg' width={110} height={110} />
         </motion.div>
-        <div className={styles.flyingMeterior}>
+        <motion.div className={styles.flyingMeterior}
+          whileHover={{
+            scale: 0,
+            transition: { duration: 3 }
+          }}
+        >
           <Image src='./images/flyingMeterior.svg' width={200} height={200} />
-        </div>
-        <div className={styles.meterior4}>
+        </motion.div>
+        <motion.div className={styles.meterior4}
+          whileHover={{
+            scale: 1.5,
+            transition: { duration: 2 },
+          }}
+        >
           <Image src='./images/half meterior3.svg' width={300} height={300} />
-        </div>
-        <div className={styles.meterior1}>
+        </motion.div>
+        <motion.div className={styles.meterior1}
+          whileHover={{
+            scale: 1.5,
+            transition: { duration: 2 },
+          }}
+        >
           <Image src='./images/meteror half.svg' width={150} height={150} />
-        </div>
+        </motion.div>
         <motion.div className={styles.planet}
           animate={{
             y: [ 0, 20, 0 ],
@@ -74,12 +89,26 @@ const Animation = () => {
         >
           <Image src='./images/satellite.svg' width={150} height={150} />
         </motion.div>
-        <div className={styles.meterior3}>
+        <motion.div className={styles.meterior3}
+          whileHover={{
+            scale: 1.5,
+            transition: { duration: 2 },
+          }}
+        >
           <Image src='./images/second half.svg' width={200} height={200} />
-        </div>
-        <div className={styles.astronaut}>
+        </motion.div>
+        <motion.div className={styles.astronaut}
+          animate={{
+            scale: [ 1, 2, 1 ],
+          }}
+          transition={{
+            duration: 30,
+            repeat: Infinity,
+          }}
+        >
           <Image src='./images/manWithSpacesuit.svg' width={90} height={90} />
-        </div>
+        </motion.div>
+        
         <div className={styles.small2}>
           <Image src='./images/small meterior 2.svg' width={110} height={110} />
         </div>
@@ -89,9 +118,14 @@ const Animation = () => {
         <div className={styles.small3}>
           <Image src='./images/small meterior 3.svg' width={110} height={110} />
         </div>
-        <div className={styles.meterior2}>
+        <motion.div className={styles.meterior2}
+          whileHover={{
+            scale: 1.5,
+            transition: { duration: 2 },
+          }}
+        >
           <Image src='./images/Untitled design (49) 4.svg' width={144} height={144} />
-        </div>
+        </motion.div>
       </div>
     </div>
   )
